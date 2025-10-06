97b14ccfac3dc4836c715e20d7be4fd7c9cf5624
AdelAlhajHussein <Adel.AlhajHussein@georgebrown.ca>
2025-10-05 23:19:54 -0400

    DetailActivity Added

---
+++ app/src/main/java/com/example/a1_adel_alhajhussein/DetailActivity.java
@@ -0,0 +1,30 @@
+package com.example.a1_adel_alhajhussein;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+
+public class DetailActivity extends AppCompatActivity {
+
+    public static ArrayList<String> paymentHistory = new ArrayList<>();
+    TextView tvHistory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        tvHistory = findViewById(R.id.tvHistory);
+
+        if (paymentHistory.isEmpty()) {
+            tvHistory.setText("No payments yet");
+        } else {
+            StringBuilder allPayments = new StringBuilder();
+            for (String record : paymentHistory) {
+                allPayments.append(record).append("\n\n");
+            }
+            tvHistory.setText(allPayments.toString());
+        }
+    }
+}
